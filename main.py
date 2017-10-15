53566a58976239d2f4d5ab3aef4aeebd3553f302
Bhishan Bhandari <bbhishan@gmail.com>
2017-10-15 08:29:01 +0545

    Exceptions handled, changes in function parameters and removal of deprecated methods.

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 import sys
-import json
-
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
 browser = webdriver.Chrome()
+browser.maximize_window()
+sheet_range = 1
 
 
-
-def connect_with_spreadsheet(clientsecrets, store, scopes=('https://www.googleapis.com/auth/spreadsheets')):
+def connect_with_spreadsheet(clientsecrets, store, scopes=('https://www.googleapis.com/auth/spreadsheets',)):
     """
     :param store:
     :param clientsecrets:
@@ -28,18 +28,22 @@ def connect_with_spreadsheet(clientsecrets, store, scopes=('https://www.googleap
     return discovery.build('sheets', 'v4', http=credz.authorize(Http()))
 
 
-def write_row_to_spreadsheet(SHEETS, sheet_ID, rows, range, valueInputOption="RAW"):
+def write_row_to_spreadsheet(sheets, sheet_id, rows, spreadsheet_range, value_input_option="RAW"):
     """
+    :param sheets:
+    :param sheet_id:
     :param rows:
+    :param spreadsheet_range:
+    :param value_input_option:
     :return:
     """
     body = {
         'values': rows
     }
-    result = SHEETS.spreadsheets().values().update(
-    spreadsheetId=sheet_ID, range=range,
-    valueInputOption=valueInputOption, body=body).execute()
-
+    result = sheets.spreadsheets().values().update(
+        spreadsheetId=sheet_id, range=spreadsheet_range,
+        valueInputOption=value_input_option, body=body).execute()
+    print(result)
 
 
 def navigate_next_page():
@@ -48,13 +52,12 @@ def navigate_next_page():
     """
     try:
         next_button = WebDriverWait(browser, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "next"))
+            ec.presence_of_element_located((By.CLASS_NAME, "next"))
         )
-    except:
+    except TimeoutException:
         print("No more pages to traverse.")
         sys.exit()
     next_button.click()
-
 
 
 def get_first_degree_email(individual_url):
@@ -62,26 +65,34 @@ def get_first_degree_email(individual_url):
     :param individual_url:
     :return:
     """
-    browser.get(individual_url)
+    js_cmd = 'window.open("' + individual_url + '", "_blank");'
+    browser.execute_script(js_cmd)
+    browser.switch_to.window(browser.window_handles[-1])
     try:
         contact_details_btn = WebDriverWait(browser, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "contact-see-more-less"))
+            ec.presence_of_element_located((By.CLASS_NAME, "contact-see-more-less"))
         )
-    except:
+    except TimeoutException:
         print("Although 1st degree connection, email not available for %s", individual_url)
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
         return ""
 
     contact_details_btn.click()
     try:
         profile_personal_section = WebDriverWait(browser, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "pv-profile-section__section-info"))
+            ec.presence_of_element_located((By.CLASS_NAME, "pv-profile-section__section-info"))
         )
-    except:
+    except TimeoutException:
         print("Can't find personal info section")
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
         return ""
 
     email_section = profile_personal_section.find_element_by_class_name("ci-email")
     email_addr = email_section.find_element_by_tag_name("a").text
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])
     return email_addr
 
 
@@ -97,7 +108,7 @@ def get_personal_info(individual):
     if connection_distance is not "1st":
         return "", individual_name, profile_url, job_title
 
-    return get_first_degree_email(profile_url), individual_name, profile_url, job_title
+    return [get_first_degree_email(profile_url), individual_name, profile_url, job_title]
 
 
 def url_constructor(base_url='https://linkedin.com/search/results/people/?origin=GLOBAL_SEARCH_HEADER', **kwargs):
@@ -111,22 +122,23 @@ def url_constructor(base_url='https://linkedin.com/search/results/people/?origin
     return base_url
 
 
-
-def search_by_filter(url):
+def search_by_filter(sheets):
     """
-    :param kwargs:
+    :param sheets:
     :return:
     """
-    browser.get(url)
+    global sheet_range
     try:
         search_result_ul = WebDriverWait(browser, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "results-list"))
+            ec.presence_of_element_located((By.CLASS_NAME, "results-list"))
         )
-    except:
+    except TimeoutException:
         print("Could not get results for the given search filters.")
         sys.exit()
 
     results_details = [get_personal_info(people) for people in search_result_ul.find_elements_by_tag_name('li')]
+    write_row_to_spreadsheet(sheets, '1_BQBY9sinWoUD1LXaYMP7rsOIr2zDgG7bUfsczYU140', results_details, 'A' + str(sheet_range))
+    sheet_range += len(results_details)
 
 
 def linkedin_login(username, password):
@@ -138,9 +150,9 @@ def linkedin_login(username, password):
     browser.get("https://linkedin.com")
     try:
         email_field = WebDriverWait(browser, 15).until(
-            EC.presence_of_element_located((By.ID, "login-email"))
+            ec.presence_of_element_located((By.ID, "login-email"))
         )
-    except:
+    except TimeoutException:
         print("The login page did not load correctly.")
         return False
     password_field = browser.find_element_by_id("login-password")
@@ -154,11 +166,22 @@ def main():
     if len(sys.argv) != 3:
         print("Please pass username and password as arguments through command line.")
         sys.exit()
+    print(sys.argv[1])
+    print(sys.argv[2])
 
     if not linkedin_login(sys.argv[1], sys.argv[2]):
         sys.exit()
 
-    search_by_filter(url_constructor(facetNetwork='["F"%2C"S"%2C"O"]', keywords='data miner%2Cscientist', company='facebook', facetNonprofitInterest='["volunteer"%2C"nonprofitboard"]'))
+    browser.get(url_constructor(facetNetwork='["F"%2C"S"%2C"O"]', keywords='data miner%2Cscientist', company='facebook',
+                                facetNonprofitInterest='["volunteer"%2C"nonprofitboard"]'))
+    sheets = connect_with_spreadsheet(
+        "client_secret_391502371883-ucq6afoq80m5h6gj3k9mur3ankfa5go6.apps.googleusercontent.com.json",
+        file.Storage('storage.json'))
+    while True:
+        search_by_filter(sheets)
+        navigate_next_page()
+
+        # search_by_filter(url_constructor(facetNetwork='["F"%2C"S"%2C"O"]', keywords='data miner%2Cscientist', company='facebook', facetNonprofitInterest='["volunteer"%2C"nonprofitboard"]'))
 
 
 if __name__ == '__main__':
@@ -175,7 +198,6 @@ Non profit interests: facetNonprofitInterest=["volunteer"%2C"nonprofitboard"]
 Origin : origin=GLOBAL_SEARCH_HEADER
 https://www.linkedin.com/search/results/people/?company=Facebook&facetNetwork=%5B%22F%22%2C%22S%22%2C%22O%22%5D&firstName=&keywords=data%20miner&lastName=Bhandari&origin=FACETED_SEARCH&title=Data%20miner
 """
-
 
 """
 Updating rows to spreadsheet.
